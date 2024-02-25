f40dd3fb817c7a2c51f5a4b075ce762d613b60a9
ardnep <aranepal@gmail.com>
2024-02-25 22:31:21 +0800

    Some yabai stuff plus lints

--- .config/nvim/lua/custom/configs/lint.lua
+++ .config/nvim/lua/custom/configs/lint.lua
@@ -1,5 +1,8 @@
 require("lint").linters_by_ft = {
 	python = { "mypy" },
+	sh = { "shellcheck" },
+	zsh = { "shellcheck" },
+	bash = { "shellcheck" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
