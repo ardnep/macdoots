8c472fd6c6a0e29640be3ef7b106963c875c7369
ardnep <aranepal@gmail.com>
2024-03-10 19:44:36 +0800

    Disable semantic tokens for tsserver

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,17 +1,26 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "eslint" }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
--- 
--- lspconfig.pyright.setup { blabla}
+-- disable semantic tokens
+lspconfig.tsserver.setup({
+	on_init = function(client, _)
+		if
+			not require("custom.chadrc").ui.lsp_semantic_tokens
+			and client.supports_method("textDocument/semanticTokens")
+		then
+			client.server_capabilities.semanticTokensProvider = nil
+		end
+	end,
+})
