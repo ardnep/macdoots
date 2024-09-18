5105d6a4d5fdf74a24ed594e24f38c98ab87b51e
ardnep <aranepal@gmail.com>
2024-09-18 16:42:59 +0800

    Fix winbar background black

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -18,7 +18,12 @@ for _, plugin in pairs(enable_providers) do
 end
 
 vim.opt.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-vim.cmd([[
-  highlight WinBar guibg=none
-  highlight WinBarNC guibg=none
-]])
+
+-- Following nvim 0.10, the default colorschemes were changed
+-- which resulted in the winbar having a black background
+-- if my sanity is still intact, this is the commit
+-- that is responsible for this change:
+-- https://github.com/neovim/neovim/commit/0c850add3ecedec5b7dea62f0dd152f3421b66fc
+-- This reverts the winbar to the previous state
+vim.api.nvim_set_hl(0, "WinBar", {})
+vim.api.nvim_set_hl(0, "WinBarNC", {})
