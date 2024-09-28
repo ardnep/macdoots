00abaeb27b14fb8c6a6a8c2f605c031efb5f6e2b
ardnep <aranepal@gmail.com>
2024-09-28 21:59:46 +0800

    Fix toggle all windows to fullscreen

--- .config/skhd/toggle_all_windows_fullscreen.zsh
+++ .config/skhd/toggle_all_windows_fullscreen.zsh
@@ -1,8 +1,16 @@
 #!/usr/bin/env zsh
 
-windows=$(yabai -m query --spaces --space mouse | jq '.windows | .[]')
-
-for window (${(f)windows}); do
-  yabai -m window $window --toggle zoom-fullscreen 
-done
-
+# if window is already in stack layout then toggle it to bsp
+if [ $(yabai -m query --spaces --space | jq '.type') = '"stack"' ]; then
+  yabai -m config layout bsp
+  yabai -m config window_placement second_child
+else 
+  yabai -m config layout stack
+  yabai -m config --space 3 layout stack
+  yabai -m space --layout stack
+  yabai -m window --focus stack.first
+  yabai -m window --focus stack.prev
+  yabai -m window --focus stack.next
+  yabai -m window --focus stack.recent
+  yabai -m window --focus stack.last
+fi
